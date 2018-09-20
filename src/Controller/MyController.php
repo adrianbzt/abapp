60f1710a0ca7e4f3b464176db6dcd211547ae06c
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2018-09-20 18:10:03 +0300

    +mines

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Services\MessageGenerator;
 
 
 class MyController extends AbstractController {
@@ -20,6 +21,23 @@ class MyController extends AbstractController {
                 'id' => 1,
                 'title' => 'Hello, React and Symfony v2',
                 'formattedPrice' => 4.99
+            ))
+        );
+    }
+
+    /**
+     * @Route("/message", name="message")
+     */
+    public function Message(MessageGenerator $messageGenerator)
+    {
+
+        $message = $messageGenerator->getHappyMessage();
+        return $this->render(
+            'base.html.twig',
+            array('event' => array(
+                'id' => 1,
+                'title' => $message,
+                'formattedPrice' => $message
             ))
         );
     }
