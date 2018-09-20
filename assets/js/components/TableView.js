ebd0c1c5cd4d892143a5cf7ecccaaa71f2e8ba17
Adrian Buzatu <adrian.buzatu@gameloft.com>
2018-09-20 17:09:35 +0300

    more work

--- assets/js/components/TableView.js
+++ assets/js/components/TableView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -13,6 +13,7 @@ import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
+import TableHead from "@material-ui/core/TableHead/TableHead";
 
 const actionsStyles = theme => ({
     root: {
@@ -43,7 +44,7 @@ class TablePaginationActions extends React.Component {
     };
 
     render() {
-        const { classes, count, page, rowsPerPage, theme } = this.props;
+        const {classes, count, page, rowsPerPage, theme} = this.props;
 
         return (
             <div className={classes.root}>
@@ -52,28 +53,28 @@ class TablePaginationActions extends React.Component {
                     disabled={page === 0}
                     aria-label="First Page"
                 >
-                    {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+                    {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
                 </IconButton>
                 <IconButton
                     onClick={this.handleBackButtonClick}
                     disabled={page === 0}
                     aria-label="Previous Page"
                 >
-                    {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                    {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
                 </IconButton>
                 <IconButton
                     onClick={this.handleNextButtonClick}
                     disabled={page >= Math.ceil(count / rowsPerPage) - 1}
                     aria-label="Next Page"
                 >
-                    {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                    {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
                 </IconButton>
                 <IconButton
                     onClick={this.handleLastPageButtonClick}
                     disabled={page >= Math.ceil(count / rowsPerPage) - 1}
                     aria-label="Last Page"
                 >
-                    {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+                    {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
                 </IconButton>
             </div>
         );
@@ -89,14 +90,16 @@ TablePaginationActions.propTypes = {
     theme: PropTypes.object.isRequired,
 };
 
-const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(
+const TablePaginationActionsWrapped = withStyles(actionsStyles, {withTheme: true})(
     TablePaginationActions,
 );
 
 let counter = 0;
-function createData(name, calories, fat) {
+
+function createData(values) {
+    let val = values.toString();
     counter += 1;
-    return { id: counter, name, calories, fat };
+    return {id: counter, val};
 }
 
 const styles = theme => ({
@@ -115,70 +118,114 @@ const styles = theme => ({
 class CustomPaginationActionsTable extends React.Component {
     state = {
         rows: [
-            createData('Cupcake', 305, 3.7),
-            createData('Donut', 452, 25.0),
-            createData('Eclair', 262, 16.0),
-            createData('Frozen yoghurt', 159, 6.0),
-            createData('Gingerbread', 356, 16.0),
-            createData('Honeycomb', 408, 3.2),
-            createData('Ice cream sandwich', 237, 9.0),
-            createData('Jelly Bean', 375, 0.0),
-            createData('KitKat', 518, 26.0),
-            createData('Lollipop', 392, 0.2),
-            createData('Marshmallow', 318, 0),
-            createData('Nougat', 360, 19.0),
-            createData('Oreo', 437, 18.0),
+            // createData('Cupcake', 305, 3.7),
+            // createData('Donut', 452, 25.0),
+            // createData('Eclair', 262, 16.0),
+            // createData('Frozen yoghurt', 159, 6.0),
+            // createData('Gingerbread', 356, 16.0),
+            // createData('Honeycomb', 408, 3.2),
+            // createData('Ice cream sandwich', 237, 9.0),
+            // createData('Jelly Bean', 375, 0.0),
+            // createData('KitKat', 518, 26.0),
+            // createData('Lollipop', 392, 0.2),
+            // createData('Marshmallow', 318, 0),
+            // createData('Nougat', 360, 19.0),
+            // createData('Oreo', 437, 18.0),
         ].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
         page: 0,
-        rowsPerPage: 5,
+        rowsPerPage: 10,
     };
 
     handleChangePage = (event, page) => {
-        this.setState({ page });
+        this.setState({page});
     };
 
     handleChangeRowsPerPage = event => {
-        this.setState({ rowsPerPage: event.target.value });
+        this.setState({rowsPerPage: event.target.value});
     };
 
+    componentDidMount() {
+        fetch("https://fbstat.mdc.gameloft.org/Odd/Api/Aria/gmd_beta.php?game=1036&kpi=2&zoom=daily&ua_options=&chart=table&platform=0&group_option=&start_date=2018-07-17&end_date=2018-07-30&country=&action=get_kpi_data")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    // console.log(result)
+                    this.setState({
+                        rows: result,
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    // this.setState({
+                    //     isLoaded: true,
+                    //     error
+                    // });
+                }
+            )
+    }
+
     render() {
-        const { classes } = this.props;
-        const { rows, rowsPerPage, page } = this.state;
+        const {classes} = this.props;
+        const {rows, rowsPerPage, page} = this.state;
         const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
 
         return (
             <Paper className={classes.root}>
                 <div className={classes.tableWrapper}>
                     <Table className={classes.table}>
-                        <TableBody>
-                            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+
+                        {rows.map((row, rowNb) => {
+                            if (rowNb === 0) {
                                 return (
-                                    <TableRow key={row.id}>
-                                        <TableCell component="th" scope="row">
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell numeric>{row.calories}</TableCell>
-                                        <TableCell numeric>{row.fat}</TableCell>
-                                    </TableRow>
-                                );
-                            })}
-                            {emptyRows > 0 && (
-                                <TableRow style={{ height: 48 * emptyRows }}>
-                                    <TableCell colSpan={6} />
-                                </TableRow>
-                            )}
-                        </TableBody>
+                                    <TableHead key="tableHead">
+                                        <TableRow key={'header-' + rowNb}>
+                                            {row.map((headerEntry, key) => {
+                                                return (
+                                                    <TableCell key={'headerColumn-' + key}> {headerEntry}</TableCell>
+
+                                                )
+                                            })}
+                                        </TableRow>
+                                    </TableHead>
+                                )
+                            }
+                            /* else {
+                                                            return (
+                                                                <TableBody>
+                                                                    {row.map((rowEntry, key) => {
+                                                                        return (
+                                                                            <TableRow key={key}>
+                                                                                <TableCell component="th" scope="row">
+                                                                                    {row}
+                                                                                </TableCell>
+                                                                            </TableRow>
+                                                                        );
+                                                                    })}
+                                                                    {emptyRows > 0 && (
+                                                                        <TableRow style={{height: 48 * emptyRows}}>
+                                                                            <TableCell colSpan={6}/>
+                                                                        </TableRow>
+                                                                    )}
+                                                                </TableBody>
+                                                            )
+
+                                                        }*/
+                        })}
+
                         <TableFooter>
                             <TableRow>
-                                <TablePagination
-                                    colSpan={3}
-                                    count={rows.length}
-                                    rowsPerPage={rowsPerPage}
-                                    page={page}
-                                    onChangePage={this.handleChangePage}
-                                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                                    ActionsComponent={TablePaginationActionsWrapped}
-                                />
+                                {/*<TablePagination*/}
+                                {/*colSpan={3}*/}
+                                {/*count={rows.length}*/}
+                                {/*rowsPerPage={rowsPerPage}*/}
+                                {/*page={page}*/}
+                                {/*onChangePage={this.handleChangePage}*/}
+                                {/*onChangeRowsPerPage={this.handleChangeRowsPerPage}*/}
+                                {/*ActionsComponent={TablePaginationActionsWrapped}*/}
+                                {/*/>*/}
                             </TableRow>
                         </TableFooter>
                     </Table>
